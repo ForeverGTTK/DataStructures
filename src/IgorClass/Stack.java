67db419693bfb75dedbcc47aa269087d35a9fd94
Guy Tuval <int.if.while@gmail.com>
2022-11-19 16:11:30 +0200

    intiate library

---
+++ src/IgorClass/Stack.java
@@ -0,0 +1,56 @@
+package IgorClass;
+
+public class Stack  {
+    private int data[] ;
+    private int stackStart=0;
+    private int nextItem =0;
+
+    public Stack (int[] data,int stackSize){
+        this (stackSize);
+        initData(data);
+        }
+
+    public Stack (int stackSize){
+        this.data=new int[stackSize];
+    }
+
+
+      private void initData(int[] data){
+        for (int i=0;i<data.length;i++)
+        push(data[i]);
+    }
+
+    public void push(int data){
+        this.data[nextItem] = data;
+        nextItem++;
+    }
+
+    public int pop (){
+        this.nextItem -= 1;
+        return this.data[nextItem+1];
+    }
+
+    public int size(){
+        return this.data.length;
+    }
+
+    public int top(){
+        return this.data[nextItem];
+    }
+
+    public boolean isEmpty (){
+        if (nextItem==stackStart)
+            return true;
+        return false;
+    }
+
+    public void printStack()
+    {
+        System.out.print("the stack contains [");
+        for (int i=0;i<data.length;i++){
+            System.out.print(data[i]+", ");
+        }
+        System.out.println("]");
+    }
+
+}
---
+++ src/IgorClass/main.java
@@ -0,0 +1,14 @@
+package IgorClass;
+
+public class main {
+
+
+
+    public static void main(String[] args) {
+        int[] data = {5, 3, 15, 4, 2, 12, 10, 4};
+        Stack stack = new Stack(data, 10);
+        stack.printStack();
+        stack.printStack();
+
+    }
+}
